6639581b4d1393d3bcf5740b3eec62abc0d3f7bc
MilonA000 <A00040557@mytudublin.ie>
2026-03-26 10:12:16 +0000

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,6 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+);
 
 console.log("SUPABASE URL IN USE:", "PASTE_THE_EXACT_PROJECT_URL_FROM_SUPABASE_HERE")"PASTE_THE_EXACT_PROJECT_URL_FROM_SUPABASE_HERE")
