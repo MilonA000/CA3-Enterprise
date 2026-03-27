377d01760f1bd9ff5086c12eebfb92a6749f695b
Evernight19 <A00028743@mytudublin.ie>
2026-03-27 13:19:03 +0000

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr"
 
-export const supabase = createClient(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
-console.log("SUPABASE URL IN USE:", "PASTE_THE_EXACT_PROJECT_URL_FROM_SUPABASE_HERE")"PASTE_THE_EXACT_PROJECT_URL_FROM_SUPABASE_HERE")
+console.log("SUPABASE URL IN USE:", process.env.NEXT_PUBLIC_SUPABASE_URL)
