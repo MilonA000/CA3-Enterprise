057cccfdf972851660f41251790273250b6fa8d0
Evernight19 <A00028743@mytudublin.ie>
2026-04-29 14:13:15 +0100

    Add files via upload

---
+++ components/BackgroundStyles.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import styles from "./Background.module.css"
+
+export default function Background() {
+  return (
+    <>
+      <div className={styles.pageBackground} />
+
+      <section className={styles.Decoration1} />
+      <section className={styles.Decoration2} />
+      <section className={styles.Decoration3} />
+
+      <section className={styles.Decoration4} />
+      <section className={styles.Decoration5} />
+      <section className={styles.Decoration6} />
+
+
+
+      <section className={styles.decoration7} />
+      <section className={styles.decoration8} />
+      <section className={styles.decoration9} />
+
+      <section className={styles.decoration10} />
+      <section className={styles.decoration11} />
+      
+    </>
+  );
+}
