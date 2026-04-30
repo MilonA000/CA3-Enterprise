085b2431eaaf0329b02395f1e99cf2df01bd5ca1
Evernight19 <A00028743@mytudublin.ie>
2026-04-30 20:38:39 +0100

    Update QuickLinks.tsx

--- components/QuickLinks.tsx
+++ components/QuickLinks.tsx
@@ -12,35 +12,35 @@ export default function QuickLinks() {
               <div className={styles.QuickLinksContent}>
                 <h3>Quick Links</h3>
 
-                <Link href="/timetable" className={styles.QuickLinksLink1}>Timetables<Image className={styles.TimetableImage1}
+                <Link href="/timetable" className={styles.QuickLinksLink1} aria-label="Timetables">Timetables<Image className={styles.TimetableImage1}
                     src="/timetable-logo.png"
                     alt="Timetable logo"
                     width={24}
                     height={24}/>
                 </Link>
 
-                <Link href="/societies" className={styles.QuickLinksLink2}>Societies<Image className={styles.SocialImage1}
+                <Link href="/societies" className={styles.QuickLinksLink2} aria-label="Societies">Societies<Image className={styles.SocialImage1}
                     src="/social-logo.png"
                     alt="Society logo"
                     width={20}
                     height={20}/>
                 </Link>
 
-                <Link href="/events" className={styles.QuickLinksLink3}>Events<Image className={styles.EventImage1}
+                <Link href="/events" className={styles.QuickLinksLink3} aria-label="Events">Events<Image className={styles.EventImage1}
                     src="/partypopper-logo.png"
                     alt="Part Popper"
                     width={20}
                     height={20}/>
                 </Link>
 
-                <Link href="/campus-map" className={styles.QuickLinksLink4}>Campus Map<Image className={styles.MapImage1}
+                <Link href="/campus-map" className={styles.QuickLinksLink4} aria-label="Campus Map">Campus Map<Image className={styles.MapImage1}
                     src="/map-logo.png"
                     alt="Map Logo"
                     width={20}
                     height={20}/>
                 </Link>
 
-                <Link href="/helpdesk" className={styles.QuickLinksLink6}>Help Desk<Image className={styles.HelpImage1}
+                <Link href="/helpdesk" className={styles.QuickLinksLink6} aria-label="Help Desk">Help Desk<Image className={styles.HelpImage1}
                     src="/help-logo.png"
                     alt="Tools logo"
                     width={20}
